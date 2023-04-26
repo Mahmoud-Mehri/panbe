9218c1024a6557aa92e12df99ef00e2cb8247a5c
Mahmoud Mehri <Mahmoud.Mehri90@gmail.com>
2023-04-26 14:02:55 +0300

    New libraries added

--- panbe.go
+++ panbe.go
@@ -7,8 +7,6 @@ import (
 	"panbe/wallet"
 )
 
-
-
 func init() {
 	// ...
 	fmt.Println("Blockchain Application Initialized");
@@ -24,8 +22,10 @@ func main() {
 	t := wallet.NewTransactionInfo(w.PrivateKey(), w.PublicKey(), w.WalletAddress(), "RecipientAddr", 2.5)
 	fmt.Printf("Signature: %s \n", t.SignTransaction())
 
-	bc := blockchain.NewBlockChain()
-	bc.AddTransaction(t, t.SignTransaction())
+	bc := blockchain.NewBlockChain("")
+	if bc.AddTransaction(t, t.SignTransaction()) {
+		bc.Print()
+	}
 
 	// bc.AddTransaction("A", "B", 2.53)
 	// bc.Mine("Miner Address")
@@ -33,7 +33,6 @@ func main() {
 	// bc.AddTransaction("B", "C", 1.4)
 	// bc.AddTransaction("C", "A", 1)
 	// bc.Mine("Miner Address")
-	bc.Print()
 
 	// fmt.Printf("Balance of Account 'B' is: %.2f\n\n", bc.CalculateAccountBalance("B"))
 }
